c2f6d674924c5ff0c53a0da16c02dac6cc0f8846
AIdoAI <41791648+AIdoAI@users.noreply.github.com>
2026-01-22 16:27:11 -0800

    Add files via upload

--- examples/programmatic_usage.py
+++ examples/programmatic_usage.py
@@ -13,9 +13,8 @@ import os
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
-from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain.schema import Document
